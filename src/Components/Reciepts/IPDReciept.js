fb24d0e590d06e564fc0cff6c81311941de0bcd2
Aman Sharma <aman@leewayhertz.com>
2021-08-18 21:50:51 +0530

    Reciepts Updated

--- src/Components/Reciepts/IPDReciept.js
+++ src/Components/Reciepts/IPDReciept.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import FirebaseFunction from '../utils/firebaseFunctions';
+import moment from 'moment';
+import Toast from '../Common/snackbar'
+import { Button } from '@material-ui/core';
+import firebase from '../utils/firebase';
+
+const useStyles = makeStyles(() => ({
+    inputReceipt: {
+        width: "100%"
+    },
+    button: {
+        background: "#238887",
+        margin: "10px",
+        color: "white",
+        '&:hover': {
+            backgroundColor: '#0C6361',
+            boxShadow: 'none',
+        },
+    },
+    fileInput: {
+        margin: "10px"
+    }
+
+}));
 
 const Container = styled.div`
     justify-content: center;
@@ -55,21 +80,22 @@ const TextHolder = styled.div`
 const HeaderInput = styled.div`
     width: 100%;
     display: flex;
-    padding: 5px 20px 5px 20px;
+    padding: 5px 20px 0px 20px;
     font-size:18px;
-    margin-top: 10px;
-
+    margin-top: 12px;
+    align-items: flex-end;
+    height:35px;
 `
 const Logo = styled.img`
     width: 70px;
     height: 80px;
     `
 const Form = styled.div`
-width: 100%;
-justify-content: center;
-flex-direction: column;
-border: 2px solid #000000;
-margin-top: 2px;
+    width: 100%;
+    justify-content: center;
+    flex-direction: column;
+    border: 2px solid #000000;
+    margin-top: 2px;
 `
 
 const HorizontalLine = styled.div`
@@ -79,55 +105,144 @@ const HorizontalLine = styled.div`
     margin-top: 10px;
 `
 const Value = styled.div`
-
+    margin-left: 25px;
+    margin-right: 35px;
+    font-weight: 500;
+    width:auto;
+    display:flex;
+    flex-grow: 1;
+`
+const ButtonContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    width:100vw;
+    padding: 30px;
+    justify-content: center;
 `
 
 
 
 function IPDReciept() {
+    const classes = useStyles();
+    useEffect(() => {
+        if (receipt.date === "") {
+            let date = Date.now();
+            setReceipt({
+                ...receipt,
+                date: moment(date).format("YYYY-MM-DDThh:mm")
+            })
+        }
+    })
+
+    const [fileNo, setFileNo] = React.useState();
+    const [patient, setPatient] = React.useState("");
+    const [selectedId, setSelectedId] = React.useState();
+    const fetchData = () => {
+        const res = FirebaseFunction.getOneData("Patients", "fileNo", fileNo);
+        console.log(res)
+        if (res.selectedId === "") Toast.apiFailureToast("Does not exist")
+        else {
+            setPatient(res.data)
+            setSelectedId(res.selectedId)
+        }
+    }
+
+    const [receipt, setReceipt] = React.useState({
+        serialNo: "", name: "", amountInWords: "", otherAmount: "", chequeNo: "", chequeDate: "", amount: "", date: ""
+    })
+    const handleInputData = (e) => {
+        setReceipt({ ...receipt, [e.target.name]: e.target.value });
+        if (e.target.name === "amount" && !isNaN(e.target.value)) {
+            let response = FirebaseFunction.toTitleCase(FirebaseFunction.inWords(e.target.value));
+            setReceipt({ ...receipt, amountInWords: response });
+        }
+    }
+
+    const SaveReceipt = () => {
+        let patientData = {
+            ...patient,receipt:receipt
+        }
+        console.log(patientData,selectedId[0])
+        const userRef = firebase.database().ref("Patients").child(selectedId[0]);
+        userRef.update(patientData).then(() => {
+            Toast.apiSuccessToast("Patient details updated")
+        }).catch(() => {
+            Toast.apiFailureToast("Server Error")
+        })
+    }
 
     return (
         <>
-        <Container>
-           <Paper>
-                <Form>
-                    <Section >
-                        <LogoAndHeading>
-                            <LogoHolder>
-                                <Logo src="/Images/Discharge.png" />
-                            </LogoHolder>
-                            <TextHolder>
-                                <Typography variant="h3">
-                                    <b>CHIRANJEEVI HOSPITAL</b>
-                                </Typography>
-                                <Typography variant="h6">
-                                    <b>Virat Sagar Parisar,Oppo. SATI College, NH-86,Vidisha (M.P.)</b><br />
-                                    <b><TelePhone src="/Images/telephone.png"/> : 250544, 251280</b>
-                                </Typography>
-                                
-                            </TextHolder>
-                        </LogoAndHeading>
-                    </Section>
-                    <HorizontalLine />
-                    <HeaderInput> 
-                        <TextField  name="text" type="text" size="small" label="No." variant="outlined"/>
-                        <TextField  style={{marginRight:"4%"}} name="text" type="text" size="small" label="Date" variant="outlined"/>
-                    </HeaderInput>
-                    <HeaderInput>
-                        Received with thanks from Mr <Value>Aman Sharma</Value>
-                    </HeaderInput>
-                    <HeaderInput>
-                    The sum of Rupees <Value> Ten Thousand Four Hundred </Value>
-                    </HeaderInput>
-                    <HeaderInput>
-                        For Hospitalization / Pathalogical Investigation / OPD / X-Ray/ECG Other
-                    </HeaderInput>
-                    <HeaderInput>
-                        Cheque No. <Value> </Value> Dated <Value>  </Value>
-                    </HeaderInput>
+            <Container>
+                <ButtonContainer>
+                    <TextField onChange={(e) => setFileNo(e.target.value)} value={fileNo} className={classes.fileInput} type="number" />
+                    <Button onClick={fetchData} className={classes.button}> Fetch </Button>
+                </ButtonContainer>
+                <Paper>
+                    <Form>
+                        <Section >
+                            <LogoAndHeading>
+                                <LogoHolder>
+                                    <Logo src="/Images/Discharge.png" />
+                                </LogoHolder>
+                                <TextHolder>
+                                    <Typography variant="h3">
+                                        <b>CHIRANJEEVI HOSPITAL</b>
+                                    </Typography>
+                                    <Typography variant="h6">
+                                        <b>Virat Sagar Parisar,Oppo. SATI College, NH-86,Vidisha (M.P.)</b><br />
+                                        <b><TelePhone src="/Images/telephone.png" /> : 250544, 251280</b>
+                                    </Typography>
+                                </TextHolder>
+                            </LogoAndHeading>
+                        </Section>
+                        <HorizontalLine />
+                        <HeaderInput style={{ justifyContent: "space-between" }}>
+                            <TextField onChange={handleInputData} name="serialNo" type="text" size="small" label="No." variant="outlined" InputLabelProps={{ shrink: true }} disabled />
+                            <TextField onChange={handleInputData} style={{ marginRight: "4%" }} name="date" type="datetime-local" InputLabelProps={{ shrink: true }} size="small" label="Date" value={receipt.date} variant="outlined" />
+                        </HeaderInput>
+                        <HeaderInput>
+                            <div>Received with thanks from {patient.sex === "M" ? "Mr" : "Mrs"} </div>
+                            <Value>
+                                <TextField className={classes.inputReceipt} name="name" type="text" size="small" label="Name" value={patient.name} InputLabelProps={{ shrink: true }} disabled />
+                            </Value>
+                        </HeaderInput>
+                        <HeaderInput>
+                            <div>The sum of Rupees </div>
+                            <Value>
+                                <TextField className={classes.inputReceipt} name="amountInWords" type="text" size="small" label="Amount In Words" value={receipt.amountInWords} InputLabelProps={{ shrink: receipt.amountInWords === "" ? false : true }} disabled />
+                            </Value>
+                        </HeaderInput>
+                        <HeaderInput style={{ fontSize: "15px" }}>
 
-                </Form>
-            </Paper>
+                            <div>For Hospitalization / Pathalogical Investigation / OPD / X-Ray / ECG Other </div>
+                            <Value>
+                                <TextField onChange={handleInputData} className={classes.inputReceipt} name="other" type="text" size="small" label="Amount" />
+                            </Value>
+                        </HeaderInput>
+                        <HeaderInput>
+                            <div>Cheque No.</div>
+                            <Value>
+                                <TextField onChange={handleInputData} className={classes.inputReceipt} name="chequeNo" type="text" size="small" label="Check No." />
+                            </Value>
+
+                            <div>Dated</div>
+                            <Value>
+                                <TextField onChange={handleInputData} className={classes.inputReceipt} name="chequeDate" type="datetime-local" size="small" label="Date" value={receipt.chequeDate} />
+                            </Value>
+                        </HeaderInput>
+                        <HeaderInput>
+                            <div><b>Amount</b></div>
+                            <Value style={{ flexGrow: "0" }}>
+                                <TextField onChange={handleInputData} className={classes.inputReceipt} name="amount" type="number" size="small" label="Amount" />
+                            </Value>
+                        </HeaderInput>
+
+                    </Form>
+                </Paper>
+                <ButtonContainer>
+                    <Button onClick={SaveReceipt} className={classes.button}> Save </Button>
+                </ButtonContainer>
             </Container>
 
         </>
--- src/Components/utils/firebaseFunctions.js
+++ src/Components/utils/firebaseFunctions.js
@@ -2,27 +2,28 @@ import firebase from "./firebase";
 
 export default {
     getOneData,
-    getData
+    getData,
+    inWords,
+    toTitleCase
 }
 
-function getOneData(db,order, id){
+function getOneData(db, order, id) {
     let selectedId = "";
-    let data =  "";
+    let data = "";
     const userRef = firebase.database().ref(db);
     var userQuery = userRef.orderByChild(order).equalTo(id);
     userQuery.once("value", function (snapshot) {
-        selectedId = Object.keys(snapshot.val())
+        if (snapshot.val()) {selectedId = Object.keys(snapshot.val());}
         snapshot.forEach(function (child) {
             data = child.val()
         });
     });
-    const response = {
-        selectedId,data
+    return {
+        selectedId, data
     }
-    return response
 }
 
-async function getData(db){
+async function getData(db) {
     const doctorRef = await firebase.database().ref(db);
     const doctorList = [];
     await doctorRef.on('value', (snap) => {
@@ -32,4 +33,28 @@ async function getData(db){
         }
     });
     return doctorList
+}
+
+var a = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '];
+var b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+function inWords(num) {
+    if ((num = num.toString()).length > 9) return 'overflow';
+    let n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+    if (!n) return; var str = '';
+    str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
+    str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
+    str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
+    str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
+    str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + 'only ' : '';
+    return str;
+}
+
+function toTitleCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function (txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
 }
